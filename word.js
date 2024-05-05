f0e8f7324815556b6e111b0128fb19ca8455f73e
NomanGhaziani <153339067+NomanGhaziani@users.noreply.github.com>
2024-05-06 04:02:16 +0500

    word-couter

---
+++ word.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+//declare a constant answers and assign it to the result of inquirer.prompt function
+const answer = await inquirer.prompt([{
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word:"
+    }]);
+const word = answer.Sentence.trim().split(" ");
+//print the array of words tothe console
+console.log(word);
+//print the words count of the sentence to the console
+console.log(`your sentence word count is  ${word.length}`);
